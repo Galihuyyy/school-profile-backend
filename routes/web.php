328c6f310a3a4ca7866753d75e21032d125c6dd4
Galihuyyy <galihputrapratama.dev@gmail.com>
2026-04-23 16:38:53 +0700

    ♻️ refactor: install tailwindcss and configure web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
+<!--
+Aturan Pemberian Route Name
+(page).(method)
+
+contoh:
+    ->name('post.create')
+
+Cara penggunaan nantinya akan seperti
+route('admin::post.create')
+-->
 <?php
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->name('admin::')->group(function() {
+
+});
+
+Route::name('user::')->group(function() {
+    Route::get('/', function() {
+        return view('welcome');
+    });
 });
