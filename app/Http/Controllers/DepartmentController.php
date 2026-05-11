ab8de5beddf798563fa0834d956a2a9d2fa24982
Galihuyyy <galihputrapratama.dev@gmail.com>
2026-05-11 11:30:42 +0700

    ✨ feat : add permission id and use id for permission department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\DepartmentRequest;
 use App\Models\Department;
 use App\Models\Teacher;
